571af27d77a0483cd6812af808d6b848f72d1b15
Sallyvely <tpdus4159@naver.com>
2022-10-26 18:59:54 +0900

    2번 문제 커밋

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,16 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+       while (true){
+            for(int i=0; i<cryptogram.length();i++){
+                if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
+                    answer=cryptogram.substring((i-1)+(i+2));
+                } else if (!(cryptogram.charAt(i)==cryptogram.charAt(i+1))){
+                    break;
+                }
+            }
+       }
         return answer;
     }
 }

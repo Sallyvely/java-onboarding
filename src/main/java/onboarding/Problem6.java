dbf70b2447cdc3a872519afe455a0205adc6f361
Sallyvely <tpdus4159@naver.com>
2022-10-27 14:34:15 +0900

    6번 문제 커밋

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,30 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        List<String> namekey= new ArrayList<>();
+        int count=0;
+        for(int i= 0; i <forms.size();i++){
+            String name=forms.get(i).get(1);
+            for(int j=0; j<name.length();j++){
+                if(forms.get(i).get(1).contains(name.substring(j,j+2))){
+                    count= count +1;
+                    if(count>1) {
+                        answer.add(forms.get(i));
+
+                    }
+
+                }
+            }
+
+        }
+
+
         return answer;
     }
 }
+S

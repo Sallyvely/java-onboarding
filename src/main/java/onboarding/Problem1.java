9746109081b2efcc3394b18adfde176e104f975c
Sallyvely <tpdus4159@naver.com>
2022-10-27 20:53:50 +0900

    1번 문제 디버그

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,59 +1,62 @@
 package onboarding;
-import java.util.List;
+import java.util.*;
+import java.lang.Integer;
+
+
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer=0;
 
-        int pobisum[]={};
-        int crongsum[]={};
-        int pobimax=0;
-        int crongmax=0;
+        ArrayList<Integer> pobilist=new ArrayList<>();
+        ArrayList<Integer> cronglist=new ArrayList<>();
 
-        for(int i=0;i<pobi.size();i++){
-            pobisum[i+2] = pobi.get(i)/100+pobi.get(i)/10+ pobi.get(i)%10;
-            if(pobi.get(i)>99){// 3자리일 경우 각 자리 곱하기
-                pobisum[i] = (pobi.get(i)/100)*(pobi.get(i)/10)*(pobi.get(i)%10);
+        //포비 페이지의 합과 곱 구하기
+        Integer[] pobiarr=pobi.toArray(new Integer[0]);
+        Integer[] crongarr=crong.toArray(new Integer[0]);
+
+        for(int i=0; i<pobiarr.length;i++){
+            int value= pobiarr[i];
+            pobilist.add(value/100+ value%100/10+ value%10);
+            if(value >99){// 3자리일 경우 각 자리 곱하기
+                pobilist.add((value/100)*(value%100/10)*(value %10));
             }
-            else pobisum[i] = (pobi.get(i)/10)*(pobi.get(i)%10);
+            else if(value < 100){
+                pobilist.add((value/10)*(value %10));}
         }
-
-        for(int i=0;i<crong.size();i++){
-            crongsum[i+2] = crong.get(i)/100+crong.get(i)/10+ crong.get(i)%10;
-            if(crong.get(i)>99){// 3자리일 경우 각 자리 곱하기
-                crongsum[i] = (crong.get(i)/100)*(crong.get(i)/10)*(crong.get(i)%10);
+        for(int j = 0; j <crongarr.length; j++){
+            int value1 = crongarr[j];
+            cronglist.add(value1/100+value1%100/10+value1%10);
+            if(value1 >99){// 3자리일 경우 각 자리 곱하기
+                cronglist.add((value1/100)*(value1%100/10)*(value1%10));
             }
-            else {
-                crongsum[i] = (crong.get(i)/10)*(crong.get(i)%10);
-            }
-        }
-
-        for(int i=0; i<pobisum.length;i++){
-            if(pobisum[i]>pobimax){
-                pobimax=pobisum[i];
-            }
-            else if(pobisum[i]==0){
-                pobimax=0;
-            }
-            if(crongsum[i]>crongmax){
-                crongmax=crongsum[i];
-            }
-            else if(crongsum[i]==0){
-                crongmax=0;
+            else if(value1 < 100){
+                cronglist.add((value1/10)*(value1%10));
             }
         }
 
-        if(crongmax>pobimax){
-            answer=2;
-        }
-        else if(pobimax>crongmax){
-            answer=1;
-        }
-        else if(pobimax==crongmax){
-            answer=0;
-        }
-        else if((pobimax|crongmax)==0){
+        int pobimax= Collections.max(pobilist);
+        int crongmax=Collections.max(cronglist);
+        int pobimin= Collections.min(pobilist);
+        int crongmin=Collections.min(cronglist);
+        System.out.println(pobilist);
+        //누가 큰지 비교하기
+        if(pobimin==0||crongmin==0){
             answer=-1;
         }
+        else{
+                if(crongmax>pobimax){
+                    answer=2;
+                }
+                else if(pobimax>crongmax){
+                    answer=1;
+                }
+                else if(pobimax==crongmax){
+                    answer=0;
+                }
+            }
+
+
+
         return answer;
     }
 }

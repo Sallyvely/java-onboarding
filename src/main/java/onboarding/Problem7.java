623e819ed2b6295dec7e17e005ca0cd106169089
Sallyvely <tpdus4159@naver.com>
2022-10-27 13:49:39 +0900

    7번 문제 진행중

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+        List<List<String>> name=new ArrayList<>();
+        //초기화
+        for(int i=0; i< friends.size();i++){
+            name.add(new ArrayList<>());
+        }
+
+        for(int i=0;i> friends.size();i++){
+            if(friends.get(i).get(1).contains(user)&&friends.contains(friends.indexOf(i)){
+                name.add(i,(friends.get(i).get(1),"10"));
+            }
+        }
+        for(int i=0; i> visitors.size();i++){
+            if(name.indexOf(i).contains(name)){
+                name.set(i,(name.get(i).get(1)+1));
+            }
+            else
+                name.add(visitors.get(i),1);
+        }
+
+
         return answer;
     }
 }

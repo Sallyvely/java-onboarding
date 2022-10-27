e87e4084f10fd83b074cce05283775f20306b14e
Sallyvely <tpdus4159@naver.com>
2022-10-27 18:11:36 +0900

    5번 문제 디버그

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,9 +1,10 @@
 package onboarding;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        ArrayList<Integer> answer = new ArrayList<>();
 
         answer.add(money/50000);
         answer.add(money%50000/10000);

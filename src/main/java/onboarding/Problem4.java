f06acf011fe4e93c4563ee9338eb2c29ea6c0813
Sallyvely <tpdus4159@naver.com>
2022-10-27 18:09:25 +0900

    4번 문제 디버그

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,10 +4,13 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         for(char x : word.toCharArray()){
-            if(x>=97 && x<=122){
-                answer=answer+(char)(219-x);
+            if(x>=97 && x<=122) {
+                answer = answer + (char) (219 - x);
             }
-            else answer=answer+(char)(155-x);
+            else if(x>=65 && x<= 97) answer=answer+(char)(155-x);
+            else
+                answer=answer+(char)x;
+
         }
         return answer;
     }

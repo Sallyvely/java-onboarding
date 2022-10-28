656e026d9e9ea10a26280eb0dc3c4c0941d7cf30
Sallyvely <tpdus4159@naver.com>
2022-10-28 16:25:56 +0900

    6번 문제 디버그

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,26 +1,36 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
-        List<String> namekey= new ArrayList<>();
-        int count=0;
-        for(int i= 0; i <forms.size();i++){
+        List<String> email=new ArrayList<>();
+        List<String> overname= new ArrayList<>();
+        Set<String> setemail =new HashSet<String>(email);
+
+        for(int i=0;i<forms.size();i++){
             String name=forms.get(i).get(1);
-            for(int j=0; j<name.length();j++){
-                if(forms.get(i).get(1).contains(name.substring(j,j+2))){
-                    count= count +1;
-                    if(count>1) {
-                        answer.add(forms.get(i).get(0));
-
-                    }
-
+            for(int j=0; j<name.length()-1;j++){
+                if(name.length()<3){
+                    break;
+                }
+                overname.add(name.substring(j,j+2));
+            }
+        }
+        for(int i=0;i<overname.size();i++){
+            for(int k=0; k< overname.size();k++){
+                if((overname.get(i).equals(overname.get(k)))&&!(i==k)){
+                    for(int j=0; j< forms.size(); j++){
+                    if(forms.get(j).get(1).contains(overname.get(i))){
+                        setemail.add(forms.get(j).get(0));
+                    }}
                 }
             }
         }
+        List<String> answeremail =new ArrayList<String>(setemail);
+
+        answer= answeremail;
         return answer;
     }
 }

de09d32799895aee5d0fe5922f3dc2b7df30c142
Sallyvely <tpdus4159@naver.com>
2022-10-29 22:01:29 +0900

    7번 문제 디버그

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,28 +6,31 @@ import java.util.stream.Collectors;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        List<List<String>> name=new ArrayList<>();
-        HashMap<String, Integer> number=new HashMap<>();
-        List<Map.Entry<String, Integer>> sortmap = number.entrySet().stream().sorted(Map.Entry.comparingByValue())
-                .collect((Collectors.toList()));
-
+        List<String> Userfriends=new ArrayList<>();
+        HashMap<String, Integer> score=new HashMap<>();
         for(int i=0; i< friends.size();i++){
-            String friendA=friends.get(i).get(0);
-            String friendB=friends.get(i).get(1);
-            if(friendB==user&&friendA.equals(friendA)){
-                number.put(friendB,+10);
+            if(score.containsKey(friends.get(i).get(1))){
+                score.put(friends.get(i).get(1),score.get(friends.get(i).get(1))+10);
+            }
+            else score.put(friends.get(i).get(1),1);
+            if(friends.get(i).get(1).contains(user)){
+                score.remove(user);
+                Userfriends.add(friends.get(i).get(0));
             }
         }
-        for(int i=0; i< visitors.size();i++){
-            number.put(visitors.get(i),+1);
+
+        for(int j=0;j< visitors.size();j++){
+            if(score.containsKey(visitors.get(j))){
+                score.put(visitors.get(j),score.get(visitors.get(j))+1);
+            } else score.put(visitors.get(j),1);
+            for(int k=0; k< Userfriends.size();k++){
+                if(score.containsKey(Userfriends.get(k))){
+                    score.remove(visitors.get(j));
+                }
+            }
         }
-        number.remove(user);
-        for(Map.Entry<String, Integer> entry : sortmap){
-                answer.add(entry.getKey());
-        }
-
-
-
+        List<String> scoreAnswer= new ArrayList<>(score.keySet());
+        answer=scoreAnswer;
         return answer;
     }
 }

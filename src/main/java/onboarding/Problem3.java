dcec4d6cae1646332015b50da392f284c49a8533
Sallyvely <tpdus4159@naver.com>
2022-10-31 14:36:07 +0900

    3번 문제 코드 리팩토링

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,24 +1,30 @@
 package onboarding;
 
+class Chk{
+    static int chk369(int num){
+        int answer=0;
+        if (num == '3') {
+            answer = answer + 1;
+        } else if (num == '6') {
+            answer = answer + 1;
+        } else if (num == '9') {
+            answer = answer + 1;
+        }
+        return answer;
+    }
+}
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
         for(int count=1;count<=number;count++){
             String str=String.valueOf(count);
+            System.out.println(str);
             for(int i=0; i<str.length();i++){
-                char chk=str.charAt(i);
-                if (chk == '3') {
-                    answer = answer + 1;
-                } else if (chk == '6') {
-                    answer = answer + 1;
-                } else if (chk == '9') {
-                    answer = answer + 1;
-                }
+                char value=str.charAt(i);
+                answer=answer+Chk.chk369(value);
             }
         }
-
-
 
 
         return answer;

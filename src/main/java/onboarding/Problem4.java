6e6b63552cfae2dc4eea6e9aaed11ebfe3147e4f
Sallyvely <tpdus4159@naver.com>
2022-10-31 14:41:27 +0900

    4번 문제 코드 리팩토링

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,16 +1,23 @@
 package onboarding;
 
+class LetterTrans{
+    static String Trans(char x){
+        String answer="";
+        if(x>=97 && x<=122) {
+            answer = answer + (char) (219 - x);
+        }
+        else if(x>=65 && x<= 97) answer=answer+(char)(155-x);
+        else
+            answer=answer+(char)x;
+        return answer;
+    }
+}
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         for(char x : word.toCharArray()){
-            if(x>=97 && x<=122) {
-                answer = answer + (char) (219 - x);
-            }
-            else if(x>=65 && x<= 97) answer=answer+(char)(155-x);
-            else
-                answer=answer+(char)x;
-
+            answer=answer + LetterTrans.Trans(x);
         }
         return answer;
     }

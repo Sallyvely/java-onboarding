ed902b2ee0afc93b64220bbc0f490ac2e64b1358
Sallyvely <tpdus4159@naver.com>
2022-10-31 15:11:43 +0900

    7번 문제 코드 리팩토링

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,16 +6,17 @@ import java.util.stream.Collectors;
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
-        List<String> Userfriends=new ArrayList<>();
+        List<String> userFriends=new ArrayList<>();
         HashMap<String, Integer> score=new HashMap<>();
         for(int i=0; i< friends.size();i++){
-            if(score.containsKey(friends.get(i).get(1))){
-                score.put(friends.get(i).get(1),score.get(friends.get(i).get(1))+10);
+            String newFriends=friends.get(i).get(1);
+            if(score.containsKey(newFriends)){
+                score.put(newFriends,score.get(newFriends)+10);
             }
-            else score.put(friends.get(i).get(1),1);
-            if(friends.get(i).get(1).contains(user)){
+            else score.put(newFriends,1);
+            if(newFriends.contains(user)){
                 score.remove(user);
-                Userfriends.add(friends.get(i).get(0));
+                userFriends.add(friends.get(i).get(0));
             }
         }
 
@@ -23,8 +24,8 @@ public class Problem7 {
             if(score.containsKey(visitors.get(j))){
                 score.put(visitors.get(j),score.get(visitors.get(j))+1);
             } else score.put(visitors.get(j),1);
-            for(int k=0; k< Userfriends.size();k++){
-                if(score.containsKey(Userfriends.get(k))){
+            for(int k=0; k< userFriends.size();k++){
+                if(score.containsKey(userFriends.get(k))){
                     score.remove(visitors.get(j));
                 }
             }
